013146fd455c00ddaf0091bfbefae9a1ef395d6e
Alves Mickaël <alves.mckl@gmail.com>
2023-09-30 13:15:56 +0200

    🇬🇧 Adding internationalisation (#20)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,47 +1,106 @@
-import { defineConfig } from 'vitepress';
+import { defineConfig, HeadConfig } from 'vitepress';
+
+interface LocaleSpecificConfig<ThemeConfig = any> {
+  lang?: string;
+  dir?: string;
+  title?: string;
+  titleTemplate?: string | boolean;
+  description?: string;
+  head?: HeadConfig[]; // will be merged with existing head entries, duplicate meta tags are automatically removed
+  themeConfig?: ThemeConfig; // will be shallow merged, common stuff can be put in top-level themeConfig entry
+}
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: 'Workshop Appwrite',
   lastUpdated: true,
-  lang: 'fr-FR',
-  description: "Il s'agit d'un atelier pour apprendre les bases d'Appwrite 🚀",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    logo: 'https://seeklogo.com/images/A/appwrite-logo-D33B39992A-seeklogo.com.png',
-    lastUpdatedText: 'Dernière mise à jour ',
-    nav: [
-      { text: 'Accueil 🏠', link: '/' },
-      { text: 'Workshop 💼', link: '/workshop/markdown-examples' },
-      { text: 'Créateurs 👨🏼‍💻', link: '/creators.md' },
-    ],
-
-    sidebar: [
-      {
-        text: '🛫 Introduction',
-        items: [
-          { text: 'Markdown Examples', link: '/workshop/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/workshop/api-examples' },
+  locales: {
+    root: {
+      label: 'FR 🇫🇷',
+      lang: 'fr',
+      title: 'Workshop Appwrite',
+      description: "Il s'agit d'un atelier pour apprendre les bases d'Appwrite 🚀",
+      themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        logo: 'https://seeklogo.com/images/A/appwrite-logo-D33B39992A-seeklogo.com.png',
+        lastUpdatedText: 'Dernière mise à jour ',
+        nav: [
+          { text: 'Accueil 🏠', link: '/' },
+          { text: 'Workshop 💼', link: '/workshop/markdown-examples' },
+          { text: 'Créateurs 👨🏼‍💻', link: '/creators.md' },
         ],
-      },
-      {
-        text: '🌋 Aventure',
-        items: [
-          { text: 'Markdown Examples', link: '/workshop/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/workshop/api-examples' },
+
+        sidebar: [
+          {
+            text: '🛫 Introduction',
+            items: [
+              { text: 'Exemple en markdown', link: '/workshop/markdown-examples' },
+              { text: "Exemples d'exécution API", link: '/workshop/api-examples' },
+            ],
+          },
+          {
+            text: '🌋 Aventure',
+            items: [
+              { text: 'Exemple en markdown', link: '/workshop/markdown-examples' },
+              { text: "Exemples d'exécution API", link: '/workshop/api-examples' },
+            ],
+          },
+          {
+            text: 'ℹ️ Informations',
+            items: [{ text: 'Créateurs 👨🏼‍💻', link: '/creators.md' }],
+          },
         ],
-      },
-      {
-        text: 'ℹ️ Informations',
-        items: [{ text: 'Créateurs 👨🏼‍💻', link: '/creators.md' }],
-      },
-    ],
 
-    socialLinks: [{ icon: 'github', link: 'https://github.com/CruuzAzul/appwrite-workshop' }],
+        socialLinks: [{ icon: 'github', link: 'https://github.com/CruuzAzul/appwrite-workshop' }],
 
-    footer: {
-      message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2023-present Mickael Alves',
+        footer: {
+          message: 'Released under the MIT License.',
+          copyright: 'Copyright © 2023-present Mickael Alves',
+        },
+      },
+    },
+    en: {
+      label: 'EN 🇬🇧',
+      lang: 'en',
+      title: 'Appwrite Workshop',
+      description: 'This is a workshop to learn the basics of Appwrite 🚀',
+      themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        logo: 'https://seeklogo.com/images/A/appwrite-logo-D33B39992A-seeklogo.com.png',
+        lastUpdatedText: 'Last update ',
+        nav: [
+          { text: 'Home 🏠', link: '/' },
+          { text: 'Workshop 💼', link: '/workshop/markdown-examples' },
+          { text: 'Creators 👨🏼‍💻', link: '/creators.md' },
+        ],
+
+        sidebar: [
+          {
+            text: '🛫 Introduction',
+            items: [
+              { text: 'Markdown Examples', link: '/workshop/markdown-examples' },
+              { text: 'Runtime API Examples', link: '/workshop/api-examples' },
+            ],
+          },
+          {
+            text: '🌋 Adventure',
+            items: [
+              { text: 'Markdown Examples', link: '/workshop/markdown-examples' },
+              { text: 'Runtime API Examples', link: '/workshop/api-examples' },
+            ],
+          },
+          {
+            text: 'ℹ️ Informations',
+            items: [{ text: 'Creators 👨🏼‍💻', link: '/creators.md' }],
+          },
+        ],
+
+        socialLinks: [{ icon: 'github', link: 'https://github.com/CruuzAzul/appwrite-workshop' }],
+
+        footer: {
+          message: 'Released under the MIT License.',
+          copyright: 'Copyright © 2023-present Mickael Alves',
+        },
+      },
     },
   },
-});
+} as LocaleSpecificConfig);
---
+++ docs/en/index.md
@@ -0,0 +1,34 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Voyage au coeur d'Appwrite :"
+  text: 'Le backend open-source qui challenge Firebase 🧳'
+  tagline: Découvrez les fondamentaux qui font d'Appwrite un outil indispensable pour tout les développeurs ! Embarquez avec nous pour cette expérience 🛫
+  actions:
+    - theme: brand
+      text: Commencer le workshop 💼
+      link: /workshop/markdown-examples
+    - theme: alt
+      text: Documentation Appwrite 📚
+      link: https://appwrite.io/docs
+
+features:
+  - icon:
+      src: assets/firebase.svg
+    title: Alternative à Firebase
+    details: Alternative à Firebase pour le développement du backend de votre applications web et mobiles.
+  - icon:
+      src: assets/open-source.svg
+    title: Open Source
+    details: Profitez d'une grande communauté de développeurs, d'une amélioration continue et d'une documentation complète.
+  - icon:
+      src: assets/hosting.svg
+    title: Auto hébergé
+    details: Autre que l'hébergement cloud ou celui en one-click, Appwrite peut être installé sur votre propre serveur.
+  - icon:
+      src: assets/dev.svg
+    title: Pour les développeurs
+    details: Conçu par des développeur et pour des développeur ! Facile à utiliser et à intégrer dans vos applications.
+---
